b94b73655d1af6b1154877c59ef1decce17135ce
Sugar-27 <45682h@gmail.com>
2021-07-05 14:44:03 +0800

    Week3

--- Week 3/2_课程表ii.cpp
+++ Week 3/2_课程表ii.cpp
@@ -6,14 +6,15 @@ class Solution {
    public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         n = numCourses;
+        //初始化出边数组
         edges = vector<vector<int>>(n, vector<int>());
         inDeg = vector<int>(n, 0);
-        for (vector<int>& pre : prerequisites) {
+        for (vector<int> pre : prerequisites) {
             int ai = pre[0], bi = pre[1];
             addEdge(bi, ai);
         }
         vector<int> ans = topsort();
-        if (ans.size() < n) {  //说明学不完
+        if (ans.size() != n) {
             return {};
         }
         return ans;
@@ -21,36 +22,31 @@ class Solution {
 
    private:
     vector<int> topsort() {
-        vector<int> order;
-        //搞个队列来排序
+        vector<int> learned;
         queue<int> q;
-        //找到入度为零的节点
         for (int i = 0; i < n; ++i) {
             if (inDeg[i] == 0) {
-                q.push(i);
+                q.push(i);  //入度为0，作为起点
             }
         }
         while (!q.empty()) {
             int x = q.front();
-            q.pop();
-            order.push_back(x);
-            for (int y : edges[x]) {
-                --inDeg[y];  //入度减一
-                if (inDeg[y] == 0) {
-                    q.push(y);
+            q.pop();  //学习了一门课
+            learned.push_back(x);
+            for (int course : edges[x]) {
+                --inDeg[course];
+                if (inDeg[course] == 0) {
+                    q.push(course);
                 }
             }
         }
-        return order;
+        return learned;
     }
-
     void addEdge(int x, int y) {
         edges[x].push_back(y);
         ++inDeg[y];
     }
-
-    int n;
+    int n;  //记录要学的课程数量
     vector<vector<int>> edges;
-    //记录入度
     vector<int> inDeg;
 };
